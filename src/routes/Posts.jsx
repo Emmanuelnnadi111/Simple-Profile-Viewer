8a9d4976981974d95750f506fa69346939bcd507
Emmanuelnnadi111 <doneonyi920@gmail.com>
2025-05-01 16:33:21 +0100

    changed from local server to render

--- src/routes/Posts.jsx
+++ src/routes/Posts.jsx
@@ -15,7 +15,9 @@ function Posts() {
 export default Posts;
 
 export async function loader() {
-     const response = await fetch("http://localhost:8080/posts");
+     const response = await fetch(
+       "https://your-backend-service.onrender.com/posts"
+     );
      const resData = await response.json();
      return resData.posts
 }
